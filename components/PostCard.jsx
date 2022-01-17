8f4e0dea09cc3dc9ea58b335542f2959da66ad09
Anthony <ahaddadkc@gmail.com>
2022-01-17 12:37:01 -0500

    front page posts and category components completed, header updated to pull true categories

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -46,12 +46,12 @@ const PostCard = ({ post }) => {
               d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
             />
           </svg>
-          <span>
-            {moment(post.createdAt).format("MMM DD, YYYY")}
-          </span>
+          <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
         </div>
       </div>
-      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">{post.excerpt}</p>
+      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
+        {post.excerpt}
+      </p>
       <div className="text-center">
         <Link href={`/post/${post.slug}`}>
           <span className="transition duration-500 transform hover:-translate-y-1 inline-bloc bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
