6cc801ab8acc040a91c45d166d73cc6059f18e67
Anthony <ahaddadkc@gmail.com>
2022-01-20 02:21:30 -0500

    small changes to Footer component, attempt to bug fix recent posts

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="container mx-auto px-10 mb-8">
-      <p className="border-t-4 text-white pt-1 bg-black bg-opacity-20 text-right">
-        Built by Unfortunate Studios
-      </p>
+      <div className="border-t-4 text-white pt-1 bg-black bg-opacity-40 text-right">
+        <a href="http://unfortunate.studio" target="_blank" className="cursor-pointer mr-2">
+          Built by Unfortunate Studios
+        </a>
+      </div>
     </footer>
   );
 };
