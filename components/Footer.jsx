f2b8f4b183b10e6db53fe63d56c5cd71e9adf63b
Anthony <ahaddadkc@gmail.com>
2022-01-20 01:24:27 -0500

    added footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="container mx-auto px-10 mb-8">
+      <p className="border-t-4 text-white pt-1 bg-black bg-opacity-20 text-right">
+        Built by Unfortunate Studios
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
